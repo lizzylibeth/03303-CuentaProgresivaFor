8f0024971e9b2553e7bcaed38d516a9788f3ff02
eli <eli@192.168.1.134>
2019-11-22 00:12:37 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,37 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Constantes
+        final int NUM_INI = 0;
+        final int NUM_FIN = 10;
+
+        //Bucle for
+        for (int i = NUM_INI; i < NUM_FIN; i++) {
+            System.out.println(i);
+        }
+
+    }//main
+
+}//class
